493646b80145a1dc5fbdfa589b231a9848076ab1
Nathaniel Strauss <nstrauss@shakopee.k12.mn.us>
2020-02-04 21:50:15 -0600

    Complete Cricut Design Space rewrite

---
+++ Cricut Design Space/CricutURLProvider.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+#
+# Copyright 2020 Nathaniel Strauss
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import re
+from xml.etree import ElementTree
+
+from autopkglib import Processor, ProcessorError, URLGetter  # noqa: F401
+
+__all__ = ["CricutURLProvider"]
+
+CRICUT_XML_URL = "http://staticcontent.cricut.com/"
+CRICUT_BASE_URL = (
+    "https://s3-us-west-2.amazonaws.com/staticcontent.cricut.com/a/"
+    + "software/osx-native/"
+)
+
+
+class CricutURLProvider(URLGetter):
+    """Provides URL to the latest Cricut Design Space version."""
+
+    description = __doc__
+    input_variables = {}
+    output_variables = {
+        "cricut_url": {"description": "Latest Cricut Design Space download URL."}
+    }
+
+    def tag_uri_and_name(self, elem):
+        if elem.tag[0] == "{":
+            uri, ignore, tag = elem.tag[1:].partition("}")
+        else:
+            uri = None
+            tag = elem.tag
+        return uri, tag
+
+    def main(self):
+        # Get release XML
+        xml = self.download(CRICUT_XML_URL)
+        xml_root = ElementTree.fromstring(xml)
+
+        # Get a list of all download URL chunks
+        url_frags = []
+        uri = self.tag_uri_and_name(xml_root)[0]
+        for frag in list(xml_root.iter("{%s}Key" % uri)):
+            if re.search("([0-9\.]+).dmg", frag.text):
+                url_frags.append(frag.text)
+
+        # Strip fragment list to get versions
+        versions = []
+        for version in url_frags:
+            try:
+                vers_split = re.split("([0-9]+)", version)[1:6]
+            except IndexError:
+                pass
+            versions.append("".join(vers_split))
+
+        if versions:
+            self.env["cricut_url"] = (
+                CRICUT_BASE_URL
+                + "Cricut%20Design%20Space%20Install%20v"
+                + (max(versions))
+                + ".dmg"
+            )
+            self.output("{}".format(self.env["cricut_url"]))
+        else:
+            raise ProcessorError("Unable to find Cricut version or URL.")
+
+
+if __name__ == "__main__":
+    processor = CricutURLProvider()
+    processor.execute_shell()
--- Cricut Design Space/README.md
+++
@@ -1,5 +0,0 @@
-Cricut Design Space installer app uses osascript in its postinstall which triggers PPPC on Mojave and above. I tried to include another postinstall script in the package originally, but it conflicted with osascript and I was unable to sort out a working PPPC profile. 
-
-As a result the postinstall isn't included, but remains in the repo since it can be used within Jamf Pro, Munki, or other management solution after package install.
-
-In Jamf Pro that would mean including the script after package install within a policy.
