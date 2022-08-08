e7045655c9ced24873ba292399c55198154e8592
nstrauss <nathaniel.strauss@gusto.com>
2022-08-08 17:03:10 -0500

    1Password preinstall

---
+++ 1Password/README.md
@@ -0,0 +1,2 @@
+# 1Password
+`preinstall.sh` will remove 1Password 7 installations. It is included by default in the Munki recipe. It is not in the .pkg recipe with the goal of keeping it as vanilla as possible. The script can be included in a Jamf policy or similar depending on your management solution before installing 1Password 8.
---
+++ 1Password/preinstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+user_id="$(scutil <<< 'show State:/Users/ConsoleUser' | awk '($1 == "Name" && $NF == "loginwindow") { exit } ($1 == "UID") { print $NF; exit }')"
+
+old_apps=(
+    "/Applications/1Password 6.app"
+    "/Applications/1Password 7.app"
+    "/Applications/1Password.app"
+)
+
+for i in "${old_apps[@]}"; do
+    if [[ -d "$i" ]]; then
+        /bin/rm -rf "$i"
+    fi
+done
+
+old_launchd=(
+    "2BUA8C4S2C.com.agilebits.onepassword7-helper"
+    "com.agilebits.onepassword7-launcher"
+)
+
+if [[ ! -z "$user_id" ]]; then
+    for l in "${old_launchd[@]}"; do
+        /bin/launchctl asuser "$user_id" /bin/launchctl stop "$l"
+        /bin/launchctl asuser "$user_id" /bin/launchctl remove "$l"
+    done
+fi
+
+/usr/bin/killall "1Password 7"
+
+exit 0
